d312a6a4385a3c9850c7e91052c830454a351e39
MrWhite2312-IS <quesada2312@gmail.com>
2026-06-19 11:31:44 -0600

    Fix

--- FrontEnd/metro.config.js
+++ FrontEnd/metro.config.js
@@ -1,6 +1,15 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+// Block the nested react-native@0.86 pulled in by @react-native/community-cli-plugin@0.86
+// to prevent codegen version mismatch (codegen 0.83 can't parse RN 0.86 component files)
+config.resolver.blockList = [
+  new RegExp(
+    path.join(__dirname, 'node_modules/react-native/node_modules/react-native').replace(/\\/g, '\\\\') + '/.*'
+  ),
+];
 
 config.resolver.resolveRequest = (context, moduleName, platform) => {
   if (moduleName.endsWith('.fx')) {
